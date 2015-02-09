5750de889d684b500bb621eb078ca9d513511a12
jaguillo <juloo.dsi@gmail.com>
2015-02-09 17:42:03 +0100

    Norme

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/11 00:38:28 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/01/18 17:11:07 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/09 16:41:52 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,71 +72,3 @@ void			draw_map(t_env *env)
 		dir += delta;
 	}
 }
-/*
-static t_pt		cast_y(t_env *env, double dir)
-{
-	t_pt			delta;
-	t_pt			pt;
-
-	delta.x = (cos(dir) > 0) ? env->map.block : -env->map.block;
-	delta.y = env->map.block * tan(dir);
-	pt.x = (env->player.pos.x) / env->map.block * env->map.block + delta.x;
-	pt.y = (env->player.pos.y - pt.x) * tan(dir) + env->player.pos.y;
-	while (1)
-	{
-		if (map_get(env, pt) != MAP_GROUND)
-			break ;
-		pt.x += delta.x;
-		pt.y += delta.y;
-	}
-	return (pt);
-}
-
-static t_pt		cast_x(t_env *env, double dir)
-{
-	t_pt			delta;
-	t_pt			pt;
-
-	delta.x = env->map.block / tan(dir);
-	delta.y = (sin(dir) > 0) ? env->map.block : -env->map.block;
-	pt.y = (env->player.pos.y) / env->map.block * env->map.block + delta.y;
-	pt.x = (env->player.pos.y - pt.y) / tan(dir) + env->player.pos.x;
-	while (1)
-	{
-		if (map_get(env, pt) != MAP_GROUND)
-			break ;
-		pt.x += delta.x;
-		pt.y += delta.y;
-	}
-	return (pt);
-}
-
-static void		draw_wall(t_env *env, int x, t_pt pt)
-{
-	int				wall;
-
-	wall = (HEIGHT - ((PROJECTION * env->map.block)
-		/ ft_dist(pt, env->player.pos))) / 2;
-	ft_drawvert(&(env->img), PT(x, 0), wall, COLOR_SKY);
-	ft_drawvert(&(env->img), PT(x, wall), HEIGHT - wall - wall, COLOR_CORNER);
-	ft_drawvert(&(env->img), PT(x, HEIGHT - wall), wall, COLOR_GROUND);
-}
-
-void			draw_map(t_env *env)
-{
-	double			delta;
-	double			dir;
-	int				x;
-
-	delta = VISION / WIDTH;
-	dir = -(VISION / 2);
-	x = 0;
-	while (x < WIDTH)
-	{
-		draw_wall(env, x, ft_nearest(env->player.pos, cast_x(env,
-			dir + env->player.dir), cast_y(env, dir + env->player.dir)));
-		x++;
-		dir += delta;
-	}
-}
-*/
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/10 23:02:35 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/01/18 17:11:48 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/09 16:41:47 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,7 @@ int				ft_dist2(t_pt p1, t_pt p2)
 	p1 = PT(p1.x - p2.x, p1.y - p2.y);
 	return ((p1.x * p1.x) + (p1.y * p1.y));
 }
-/*
-t_pt			ft_farthest(t_pt pos, t_pt p1, t_pt p2)
-{
-	const t_pt		t1 = PT(p1.x - pos.x, p1.y - pos.y);
-	const t_pt		t2 = PT(p2.x - pos.x, p2.y - pos.y);
 
-	if (((t1.x * t1.x) + (t1.y * t1.y)) > ((t2.x * t2.x) + (t2.y * t2.y)))
-		return (p1);
-	return (p2);
-}
-*/
 t_pt			ft_nearest(t_pt pos, t_pt p1, t_pt p2)
 {
 	const t_pt		t1 = PT(p1.x - pos.x, p1.y - pos.y);
